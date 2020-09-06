fabcf6c27636bdd049fa53f85d76da913ce487b3
Koji Wakamiya <koji.wakamiya@gmail.com>
2020-09-06 16:47:12 +0900

    Change function to module

--- src/index.ts
+++ src/index.ts
@@ -11,68 +11,70 @@ import { execSync } from 'child_process'
 import { readFileSync } from 'fs'
 import { parseStringPromise } from 'xml2js'
 
-export interface IPluginConfig {
+export type IPluginConfig = {
     skipTask: boolean
     task: string
     lintResultPath: string
 }
 
-export default async function android(config: IPluginConfig): Promise<void> {
-    const isGradleExist = execSync('ls gradlew').buffer.byteLength != 0
-    if (!isGradleExist) {
-        fail('Could not found gradlew.')
-        return
-    }
-
-    // run android lint by gradle task
-    if (config?.skipTask !== true) {
-        const task = config?.task ?? 'lint'
-        execSync(`gradlew ${task}`)
-    }
-
-    // find lint-result.xml
-    const path = config?.lintResultPath ?? 'app/build/reports/lint/lint-result.xml'
-    const lintRaw = readFileSync(path, 'utf-8')
-    if (lintRaw == null || lintRaw.length == 0) {
-        fail('Could not found result file of lint.')
-        return
-    }
-
-    // parse xml to json
-    const json = await parseStringPromise(lintRaw)
-    const issues: Issues = Object.assign({}, json)
-
-    // check file
-    const editFiles = danger.git.modified_files.filter(element => !danger.git.deleted_files.includes(element))
-    const createFiles = danger.git.created_files
-    const files = [...editFiles, ...createFiles]
-    const dir = process.env.PWD
-
-    issues.issues.forEach(issue => {
-        const location = issue.location
-        const filename = (location.file).replace(dir, '')
-        if (!files.includes(filename)) {
+module.exports = {
+    androidlint: async (config: IPluginConfig) => {
+        const isGradleExist = execSync('ls gradlew').buffer.byteLength != 0
+        if (!isGradleExist) {
+            fail('Could not found gradlew.')
             return
         }
 
-        const line = parseInt(location['line'] ?? '0')
-        send(issue.severity, issue.message, filename, line)
-    })
+        // run android lint by gradle task
+        if (config?.skipTask !== true) {
+            const task = config?.task ?? 'lint'
+            execSync(`gradlew ${task}`)
+        }
 
-    function send(severity: string, messageText: string, file: string, line: number) {
-        switch (severity) {
-            case Severity.WARNING:
-                warn(messageText, file, line)
-                break
-            case Severity.INFORMATIONAL:
-                message(messageText, file, line)
-                break
-            case Severity.FATAL:
-            case Severity.ERROR:
-                fail(messageText, file, line)
-                break
-            default:
-            // nop
+        // find lint-result.xml
+        const path = config?.lintResultPath ?? 'app/build/reports/lint/lint-result.xml'
+        const lintRaw = readFileSync(path, 'utf-8')
+        if (lintRaw == null || lintRaw.length == 0) {
+            fail('Could not found result file of lint.')
+            return
+        }
+
+        // parse xml to json
+        const json = await parseStringPromise(lintRaw)
+        const issues: Issues = Object.assign({}, json)
+
+        // check file
+        const editFiles = danger.git.modified_files.filter(element => !danger.git.deleted_files.includes(element))
+        const createFiles = danger.git.created_files
+        const files = [...editFiles, ...createFiles]
+        const dir = process.env.PWD
+
+        issues.issues.forEach(issue => {
+            const location = issue.location
+            const filename = (location.file).replace(dir, '')
+            if (!files.includes(filename)) {
+                return
+            }
+
+            const line = parseInt(location['line'] ?? '0')
+            send(issue.severity, issue.message, filename, line)
+        })
+
+        function send(severity: string, messageText: string, file: string, line: number) {
+            switch (severity) {
+                case Severity.WARNING:
+                    warn(messageText, file, line)
+                    break
+                case Severity.INFORMATIONAL:
+                    message(messageText, file, line)
+                    break
+                case Severity.FATAL:
+                case Severity.ERROR:
+                    fail(messageText, file, line)
+                    break
+                default:
+                // nop
+            }
         }
     }
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,1 @@
-export default async function android(config: IPluginConfig): Promise<void>
 
-export interface IPluginConfig {
-    skipTask: boolean
-    task: string
-    lintResultPath: string
-}
